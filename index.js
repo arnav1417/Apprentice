b26a208978e66d4c952b7ea8fc5c44b77945704e
Arnav <arnav27ahuja@gmail.com>
2024-12-17 17:02:03 +0530

    The core functionality of bot interaction

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Client, Collection,Events, GatewayIntentBits} = require("discord.js");
+const dotenv = require("dotenv").config().parsed;
+const token = dotenv.token;
+const fs = require("node:fs");
+const path = require("node:path");
+const client = new Client({intents:[GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
+
+const folderPath = path.join(__dirname, 'Commands');
+const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+     const filepath = path.join(folderPath, file);
+     const command = require(filepath);
+     if('data' in command && 'execute' in command){
+          client.commands.set(command.data.name, command);
+     }else{
+          console.log('[Warning]');
+     }
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(token);
