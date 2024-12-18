a8f71357be3b053552ca606bc32d01aa95bc398e
Arnav <arnav27ahuja@gmail.com>
2024-12-19 00:42:07 +0530

    Command to list containers

---
+++ Commands/dockerContainerList.js
@@ -0,0 +1,28 @@
+const {SlashCommandBuilder} = require("discord.js");
+const {listContainer} = require("../Helper_function/list_Container"); 
+module.exports = {
+    data : new SlashCommandBuilder()
+     .setName('docker-list')
+    .setDescription('List Docker containers')
+    .addStringOption(option =>
+      option.setName('container')
+        .setDescription('Container to list')
+        .setRequired(false)
+    ),
+    async execute(interaction){
+        await interaction.deferReply();
+        const containerName = interaction.options.getString('container');
+        if(containerName){
+            const output = await listContainer(containerName);
+            if(output.Complete) await interaction.editReply(`${output.Complete}`);
+            else if(output.stderr) await interaction.editReply(`${output.stderr}`);
+            else await interaction.editReply(`Error in listing ${containerName}`);
+        }
+        else{
+            const output = await listContainer();
+            if(output.Complete) await interaction.editReply(`${output.Complete}`);
+            else if(output.stderr) await interaction.editReply(`${output.stderr}`);
+            else await interaction.editReply(`Error in listing`);
+        }
+    }
+}
