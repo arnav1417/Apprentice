fbdba41310a5efb68c0b7c08acd000ec4dbc4c6c
arnav1417 <arnav27ahuja@gmail.com>
2024-12-20 11:52:31 +0530

    Shows a list of containers in table

--- Commands/dockerContainerList.js
+++ Commands/dockerContainerList.js
@@ -1,24 +1,24 @@
-const {SlashCommandBuilder} = require("discord.js");
-const {listContainer} = require("../Helper_function/list_Container"); 
+const { SlashCommandBuilder } = require("discord.js");
+const { listContainer } = require("../Helper_function/list_Container");
 module.exports = {
-    data : new SlashCommandBuilder()
-     .setName('docker-list')
-    .setDescription('List Docker containers')
-    .addStringOption(option =>
-      option.setName('container')
-        .setDescription('Container to list')
-        .setRequired(false)
-    ),
-    async execute(interaction){
+    data: new SlashCommandBuilder()
+        .setName('docker-list')
+        .setDescription('List Docker containers')
+        .addStringOption(option =>
+            option.setName('container')
+                .setDescription('Container to list')
+                .setRequired(false)
+        ),
+    async execute(interaction) {
         await interaction.deferReply();
         const containerName = interaction.options.getString('container');
-        if(containerName){
+        if (containerName) {
             const output = await listContainer(containerName);
-            if(output.Complete) await interaction.editReply(`${output.Complete}`);
-            else if(output.stderr) await interaction.editReply(`${output.stderr}`);
+            if (output.Complete) await interaction.editReply(`${output.Complete}`);
+            else if (output.stderr) await interaction.editReply(`${output.stderr}`);
             else await interaction.editReply(`Error in listing ${containerName}`);
         }
-        else{
+        else {
             const output = await listContainer();
             if(output.Complete) await interaction.editReply(`${output.Complete}`);
             else if(output.stderr) await interaction.editReply(`${output.stderr}`);
