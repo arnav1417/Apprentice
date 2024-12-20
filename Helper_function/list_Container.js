a96dad058732477ddd7f429d7542ba7d41c8425b
arnav1417 <arnav27ahuja@gmail.com>
2024-12-20 11:53:22 +0530

    Helper: List Containers

--- Helper_function/list_Container.js
+++ Helper_function/list_Container.js
@@ -1,30 +1,39 @@
 const { exec } = require("child_process");
-exports.listContainer=(ContainerName)=>{
-    return new Promise ((resolve)=>{
+const table = require("text-table");
+exports.listContainer = (ContainerName) => {
+    return new Promise((resolve) => {
         // findstr for windows
-        if(ContainerName) exec(`docker ps | grep ${ContainerName}`,  (error, stdout, stderr) => {
-            if(stderr){
-              resolve({"stderr":stderr});
-          }
-          else if (stdout){
-              resolve({"Complete":stdout});
-          }
-          else{
-              resolve({"error":error});
-          }
-          } )
-          else{
-            exec(`docker ps`,  (error, stdout, stderr) => {
-                if(stderr){
-                  resolve({"stderr":stderr});
-              }
-              else if (stdout){
-                  resolve({"Complete":stdout});
-              }
-              else{
-                  resolve({"error":error});
-              }
-              } )
-          }
+        if (ContainerName) exec(`docker compose ls | grep ${ContainerName}`, (error, stdout, stderr) => {
+            if (stderr) {
+                resolve({ "stderr": stderr });
+            }
+            else if (stdout) {
+                resolve({ "Complete": stdout });
+            }
+            else {
+                resolve({ "error": error });
+            }
+        })
+        else {
+            exec(`docker compose ls --format json`, (error, stdout, stderr) => {
+                if (stderr) {
+                    resolve({ "stderr": stderr });
+                }
+                else if (stdout) {
+                    const tab = Object.values(JSON.parse(stdout)).map((values) => {
+                        return (
+                            [`${values.Name}`, `${values.Status}`]
+                        )
+                    })
+                    var newtab =
+                        "Name" + "            " + "Status\n" +
+                        `${table(tab)}`
+                    resolve({ "Complete": newtab });
+                }
+                else {
+                    resolve({ "error": error });
+                }
+            })
+        }
     })
 }
