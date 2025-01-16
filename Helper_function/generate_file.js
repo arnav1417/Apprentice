5f8163f5fd44c470bbc1cb4198bd78cdae653add
arnav1417 <arnav27ahuja@gmail.com>
2025-01-16 18:25:23 +0530

    Helper: Location in Env

--- Helper_function/generate_file.js
+++ Helper_function/generate_file.js
@@ -1,7 +1,9 @@
 const { exec } = require("child_process");
+const path = require("path");
+const file_loc = require("dotenv").config({path:path.join(__dirname,'../','.env')}).parsed.file_loc;
 exports.generateFile = () => {
     return new Promise((resolve) => {
-        exec(`docker compose ls --format json | tee file_config.json`, (error, stdout, stderr) => {
+        exec(`docker compose ls --format json | tee ${file_loc}`, (error, stdout, stderr) => {
             if (error) {resolve({"error":error}) }
             else if(stdout) {resolve({"Complete":true}) }
         })
