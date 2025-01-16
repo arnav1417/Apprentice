e9b8b5766f2c0b85768fd34b4bb3f9efe203ce49
arnav1417 <arnav27ahuja@gmail.com>
2025-01-16 18:35:28 +0530

    Cleaned up some code

--- Commands/sample.js
+++ Commands/sample.js
@@ -1,12 +1,9 @@
 const {SlashCommandBuilder} = require("discord.js");
-const { execute } = require("./test");
-
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('test')
     .setDescription("prints text in console"),
     async execute(interaction){
-        console.log("hello");
         await interaction.reply("Done");
     }
 }
